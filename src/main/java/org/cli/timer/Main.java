81a4bce4526393b6d80f08dfc7ffadd3541940eb
Alessandro Somigli <alessandro.somigli.04@gmail.com>
2023-01-27 18:12:51 +0100

    first commit

---
+++ src/main/java/org/cli/timer/Main.java
@@ -0,0 +1,13 @@
+package org.cli.timer;
+
+import java.util.Arrays;
+
+// -t (time): time in seconds. default is 600
+// -d (delay): defines how many seconds before running again. default is -1 (do not run again)
+// -s (silent): weather to show a popup when finished. default is false
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
