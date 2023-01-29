5456df55efda25c7d1bad740deef6011d8f3fff9
Alessandro Somigli <alessandro.somigli.04@gmail.com>
2023-01-29 02:08:44 +0100

    added ascii art

--- src/main/java/org/cli/timer/CLI.java
+++ src/main/java/org/cli/timer/CLI.java
@@ -1,13 +1,16 @@
 package org.cli.timer;
 
+import org.cli.timer.utils.AsciiArt;
 import org.cli.timer.utils.Utils;
 
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class CLI {
-    private final int progressBarLength = 50;
+    private final int progressBarLength = 100;
     private final Timer timer;
 
     private static final File startSoundFile = new File("./sound-effects/start-timer.wav").getAbsoluteFile();
@@ -15,36 +18,12 @@ public class CLI {
 
     public CLI(Timer _timer) {
         timer = _timer;
-            timer.setOnTimerTick(progress -> this.generate(progress, timer.getTime(), timer.getStatus()));
-            timer.setOnDelayTick(progress -> this.generate(progress, timer.getDelay(), timer.getStatus()));
+            timer.setOnTimerTick(progress -> this.template(progress, timer.getTime(), timer.getStatus()));
+            timer.setOnDelayTick(progress -> this.template(progress, timer.getDelay(), timer.getStatus()));
 
             timer.setOnPlayStartSound( () -> new Thread(() -> this.sound(startSoundFile)).start() );
 
             timer.setOnPlayRingSound(() -> new Thread(() -> this.sound(timerRingFile)).start() );
-    }
-
-    public void generate(int _progress, int _max, String _status) {
-        String timerProgressLine = ((_status.equals("TIMER"))? "Timer":"Delay") + " progress: " + _progress + " of " + _max;
-
-        int progressChars = (int)(( (double)_progress/_max ) * progressBarLength);
-        String timerProgressBarFill = new String(new char[progressChars]).replace('\0', '█');
-        String timerProgressBarEmpty = new String(new char[progressBarLength-progressChars]).replace('\0', ' ');
-
-        String timerProgressBar = '[' + timerProgressBarFill + timerProgressBarEmpty + ']';
-
-        template(timerProgressLine, timerProgressBar);
-    }
-
-    // width: 54
-    public void template(String _timerProgressLine, String _timerProgressBar) {
-        // cls
-        try { new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); }
-        catch (InterruptedException | IOException e) { Utils.log(e.getMessage()); }
-
-        Utils.print("Timer CLI"     + "\n\n" +
-                _timerProgressLine  + '\n' +
-                _timerProgressBar   + '\n'
-        );
     }
 
     public void sound(File _file) {
@@ -62,4 +41,91 @@ public class CLI {
         timer.start();
     }
 
+    // width: 54
+    public void template(int _progress, int _max, String _status) {
+        // cls
+        try { new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor(); }
+        catch (InterruptedException | IOException e) { Utils.log(e.getMessage()); }
+
+        String title = AsciiArt.toString( AsciiArt.getTitle() );
+
+        String mode = ((_status.equals("TIMER"))?
+                AsciiArt.toString( AsciiArt.getModeTimer() ):
+                AsciiArt.toString( AsciiArt.getModePause() ) );
+
+        String progress = AsciiArt.toString( AsciiArt.appendString(
+            this.generatePercentage( (int)(( (double)_progress/_max ) * 100) ),
+            this.generateTimer(_progress, _max)
+        ) );
+
+        String bar = AsciiArt.toString( generateProgressBar(_progress, _max) );
+
+        Utils.print(
+                title +
+                mode  +
+                progress +
+                bar
+        );
+    }
+
+    public String[] generatePercentage(int _number) {
+        String[] title = AsciiArt.getProgress();
+
+        String[] numbers = new String[title.length];
+        Arrays.fill(numbers, "");
+        do {
+            String[] asciiNumber = AsciiArt.getNumber(_number % 10);
+            numbers = AsciiArt.appendString(asciiNumber, numbers);
+            _number = _number / 10;
+        } while (_number > 0);
+        AsciiArt.appendString(title, numbers);
+        AsciiArt.appendString(title, AsciiArt.getPercent());
+
+        return title;
+    }
+
+    public String[] generateTimer(int _progress, int _max) {
+        String[] title = new String[6];
+        Arrays.fill(title, "  ");
+
+        AsciiArt.appendString(title, AsciiArt.getOpenBrackets());
+
+        String[] progressNumber = new String[title.length];
+        Arrays.fill(progressNumber, "");
+       do {
+            String[] asciiNumber = AsciiArt.getNumber(_progress % 10);
+            progressNumber = AsciiArt.appendString(asciiNumber, progressNumber);
+            _progress = _progress / 10;
+        }  while (_progress > 0);
+
+        String[] maxNumber = new String[title.length];
+        Arrays.fill(maxNumber, "");
+        do {
+            String[] asciiNumber = AsciiArt.getNumber(_max % 10);
+            maxNumber = AsciiArt.appendString(asciiNumber, maxNumber);
+            _max = _max / 10;
+        } while (_max > 0);
+
+        AsciiArt.appendString(title, progressNumber);
+        AsciiArt.appendString(title, AsciiArt.getOf());
+        AsciiArt.appendString(title, maxNumber);
+
+        AsciiArt.appendString(title, AsciiArt.getCloseBrackets());
+
+        return title;
+    }
+
+    public String[] generateProgressBar(int _progress, int _max) {
+        int progressChars = (int)(( (double)_progress/_max ) * progressBarLength);
+        String timerProgressBarFill = new String(new char[progressChars]).replace('\0', '█');
+        String timerProgressBarEmpty = new String(new char[progressBarLength-progressChars]).replace('\0', ' ');
+
+        String[] title = new String[6];
+
+        Arrays.fill(title, timerProgressBarFill + timerProgressBarEmpty);
+        title = AsciiArt.appendString(AsciiArt.getOpenBrackets(), title);
+        AsciiArt.appendString(title, AsciiArt.getCloseBrackets());
+
+        return title;
+    }
 }
---
+++ src/main/java/org/cli/timer/utils/AsciiArt.java
@@ -0,0 +1,206 @@
+package org.cli.timer.utils;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class AsciiArt {
+
+    public static @NotNull String toString(String @NotNull [] ascii) {
+        StringBuilder result = new StringBuilder();
+        for (String line : ascii) { result.append(line).append('\n'); }
+        return result.toString();
+    }
+
+    @Contract("_, _ -> param1")
+    public static String @NotNull [] appendString(String @NotNull [] string1, String @NotNull [] string2) {
+        int len = Math.min(string1.length, string2.length);
+        for (int i = 0; i < len; i++) { string1[i] += string2[i]; }
+        return string1;
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getTitle() {
+        return new String[]{
+                "╔══════════════════════════════════════════════════════════════╗",
+                "║ ████████╗██╗███╗░░░███╗███████╗██████╗░  ░█████╗░██╗░░░░░██╗ ║",
+                "║ ╚══██╔══╝██║████╗░████║██╔════╝██╔══██╗  ██╔══██╗██║░░░░░██║ ║",
+                "║ ░░░██║░░░██║██╔████╔██║█████╗░░██████╔╝  ██║░░╚═╝██║░░░░░██║ ║",
+                "║ ░░░██║░░░██║██║╚██╔╝██║██╔══╝░░██╔══██╗  ██║░░██╗██║░░░░░██║ ║",
+                "║ ░░░██║░░░██║██║░╚═╝░██║███████╗██║░░██║  ╚█████╔╝███████╗██║ ║",
+                "║ ░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝  ░╚════╝░╚══════╝╚═╝ ║",
+                "╚══════════════════════════════════════════════════════════════╝"
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getModeTimer() {
+        return new String[]{
+                "  ╔═╗╔═╗    ╔╗     ╔════╦══╦═╗╔═╦═══╦═══╗",
+                "  ║║╚╝║║    ║║     ║╔╗╔╗╠╣╠╣║╚╝║║╔══╣╔═╗║",
+                "  ║╔╗╔╗╠══╦═╝╠══╗╔╗╚╝║║╚╝║║║╔╗╔╗║╚══╣╚═╝║",
+                "  ║║║║║║╔╗║╔╗║║═╣╚╝  ║║  ║║║║║║║║╔══╣╔╗╔╝",
+                "  ║║║║║║╚╝║╚╝║║═╣╔╗  ║║ ╔╣╠╣║║║║║╚══╣║║╚╗",
+                "  ╚╝╚╝╚╩══╩══╩══╝╚╝  ╚╝─╚══╩╝╚╝╚╩═══╩╝╚═╝"
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getModePause() {
+        return new String[]{
+                "  ╔═╗╔═╗    ╔╗     ╔═══╦═══╦╗ ╔╦═══╦═══╗                        ",
+                "  ║║╚╝║║    ║║     ║╔═╗║╔═╗║║ ║║╔═╗║╔══╝                        ",
+                "  ║╔╗╔╗╠══╦═╝╠══╗╔╗║╚═╝║║ ║║║ ║║╚══╣╚══╗                        ",
+                "  ║║║║║║╔╗║╔╗║║═╣╚╝║╔══╣╚═╝║║ ║╠══╗║╔══╝                        ",
+                "  ║║║║║║╚╝║╚╝║║═╣╔╗║║  ║╔═╗║╚═╝║╚═╝║╚══╗                        ",
+                "  ╚╝╚╝╚╩══╩══╩══╝╚╝╚╝  ╚╝ ╚╩═══╩═══╩═══╝                        "
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getProgress() {
+        return new String[]{
+                "  ╔═══╗                      ",
+                "  ║╔═╗║                      ",
+                "  ║╚═╝╠═╦══╦══╦═╦══╦══╦══╗╔╗ ",
+                "  ║╔══╣╔╣╔╗║╔╗║╔╣║═╣══╣══╣╚╝ ",
+                "  ║║  ║║║╚╝║╚╝║║║║═╬══╠══║╔╗ ",
+                "  ╚╝  ╚╝╚══╩═╗╠╝╚══╩══╩══╝╚╝ ",
+                "           ╔═╝║              ",
+                "           ╚══╝              "
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getPercent() {
+        return new String[]{
+                "╔═╗   ╔╗",
+                "╚═╝ ╔═╝║",
+                "  ╔═╝╔═╝",
+                "╔═╝╔═╝  ",
+                "║╔═╝ ╔═╗",
+                "╚╝   ╚═╝"
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getOpenBrackets() {
+        return new String[]{
+                "╔═ ",
+                "║  ",
+                "║  ",
+                "║  ",
+                "║  ",
+                "╚═ "
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getCloseBrackets() {
+        return new String[]{
+                " ═╗",
+                "  ║",
+                "  ║",
+                "  ║",
+                "  ║",
+                " ═╝"
+        };
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static String @NotNull [] getOf() {
+        return new String[]{
+                "     ╔═╗ ",
+                "     ║╔╝ ",
+                " ╔══╦╝╚╗ ",
+                " ║╔╗╠╗╔╝ ",
+                " ║╚╝║║║  ",
+                " ╚══╝╚╝  "
+        };
+    }
+
+    public static String[] getNumber(int _number) {
+        return switch (_number) {
+            case 0 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "║║║║║",
+                    "║║║║║",
+                    "║╚═╝║",
+                    "╚═══╝"
+            };
+            case 1 -> new String[]{
+                    " ╔╗ ",
+                    "╔╝║ ",
+                    "╚╗║ ",
+                    " ║║ ",
+                    "╔╝╚╗",
+                    "╚══╝"
+            };
+            case 2 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "╚╝╔╝║",
+                    "╔═╝╔╝",
+                    "║║╚═╗",
+                    "╚═══╝"
+            };
+            case 3 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "╚╝╔╝║",
+                    "╔╗╚╗║",
+                    "║╚═╝║",
+                    "╚═══╝"
+            };
+            case 4 -> new String[]{
+                    "╔╗ ╔╗",
+                    "║║ ║║",
+                    "║╚═╝║",
+                    "╚══╗║",
+                    "   ║║",
+                    "   ╚╝"
+            };
+            case 5 -> new String[]{
+                    "╔═══╗",
+                    "║╔══╝",
+                    "║╚══╗",
+                    "╚══╗║",
+                    "╔══╝║",
+                    "╚═══╝"
+            };
+            case 6 -> new String[]{
+                    "╔═══╗",
+                    "║╔══╝",
+                    "║╚══╗",
+                    "║╔═╗║",
+                    "║╚═╝║",
+                    "╚═══╝"
+            };
+            case 7 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "╚╝╔╝║",
+                    "  ║╔╝",
+                    "  ║║ ",
+                    "  ╚╝ "
+            };
+            case 8 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "║╚═╝║",
+                    "║╔═╗║",
+                    "║╚═╝║",
+                    "╚═══╝"
+            };
+            case 9 -> new String[]{
+                    "╔═══╗",
+                    "║╔═╗║",
+                    "║╚═╝║",
+                    "╚══╗║",
+                    "╔══╝║",
+                    "╚═══╝"
+            };
+            default -> new String[]{"", "", "", "", "", ""};
+        };
+    }
+}
